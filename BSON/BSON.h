0437a77779bd831347b9a19c1bf479138e9970b8
Joannis Orlandos <joannis@orlandos.nl>
2016-04-28 01:11:42 +0200

    Added Cocoa Framework target

---
+++ BSON/BSON.h
@@ -0,0 +1,19 @@
+//
+//  BSON.h
+//  BSON
+//
+//  Created by Joannis Orlandos on 28/04/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BSON.
+FOUNDATION_EXPORT double BSONVersionNumber;
+
+//! Project version string for BSON.
+FOUNDATION_EXPORT const unsigned char BSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BSON/PublicHeader.h>
+
+
